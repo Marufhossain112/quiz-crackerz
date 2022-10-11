0187d075c5ec3dff2c02864155c5e9d30e91a655
maruf <dev.maruf112@gmail.com>
2022-10-12 00:12:51 +0600

    eye icon has been added

--- src/components/Quizdetails/Quizdetails.js
+++ src/components/Quizdetails/Quizdetails.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Quizoption from "../Quizoption/Quizoption";
+import icon from "./icon/eye.svg";
 
 const Quizdetails = ({ quiz }) => {
   const { correctAnswer, id, question, options } = quiz;
@@ -16,8 +16,9 @@ const Quizdetails = ({ quiz }) => {
     }
   };
   return (
-    <div className="bg-violet-50">
-      <h3 className="text-xl m-5 font-bold pt-5">{question}</h3>
+    <div className="bg-violet-50 relative">
+      <h3 className="text-xl m-[15px] font-bold pt-5">{question}</h3>
+      <img className="absolute left-[85%] top-[7%]" src={icon} alt="" />
       <div className="pb-5">
         {options.map((option) => (
           <div className="flex justify-center">
