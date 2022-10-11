ff86ddb58547584ff86a580dc733591b0e0d41e0
maruf <dev.maruf112@gmail.com>
2022-10-11 16:21:22 +0600

    useNavigate added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,18 @@
 import React from "react";
-import "./Home.css"
-import Homedetails from "../Homedetails/Homedetails"
-import {
-  useLoaderData
-} from "react-router-dom";
+import "./Home.css";
+import Homedetails from "../Homedetails/Homedetails";
+import { useLoaderData } from "react-router-dom";
 const Home = () => {
   const data = useLoaderData().data;
   // console.log(data);
   return (
     <div>
-        <div className = "courses-container">
-      {data.map(item=><Homedetails key={item.id} item={item} />
-    )}
+      <div className="courses-container">
+        {data.map((item) => (
+          <Homedetails key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-    </div>
-
-
   );
 };
 
--- src/components/Homedetails/Homedetails.js
+++ src/components/Homedetails/Homedetails.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 const Homedetails = ({ item }) => {
+  const navigate = useNavigate();
   const { id, name, logo, total } = item;
+  const handleNavigate = () => {
+    navigate(`/quiz/${id}`);
+  };
   return (
     <div className="border rounded-md p-5">
       <h5 className="font-bold text-3xl">{name}</h5>
-      <img style={{ height: "200px", width: "200px" }} className="mx-auto" src={logo} />
+      <img
+        style={{ height: "200px", width: "200px" }}
+        className="mx-auto"
+        src={logo}
+      />
       <p className="font-bold mt-1">Total : {total}</p>
-      <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-2">
+      <button
+        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-2"
+        onClick={handleNavigate}
+      >
         Explore
       </button>
     </div>
