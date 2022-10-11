732ef158dfcdf579bf860b52e24d479fb3753f28
maruf <dev.maruf112@gmail.com>
2022-10-11 10:21:28 +0600

    loader has been added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is home</h2>
-        </div>
-    );
+  const data = useLoaderData().data;
+  // console.log(data);
+  return (
+    <div>
+      <h2>This is home</h2>
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,10 +9,12 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+
     errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
+        loader: () => fetch("https://openapi.programming-hero.com/api/quiz"),
         element: <Home></Home>,
       },
       {
