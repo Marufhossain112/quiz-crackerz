a21510dc78c620394f0fd2efb8b3a0d1e4883424
maruf <dev.maruf112@gmail.com>
2022-10-11 14:23:35 +0600

    explore button added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Home.css"
 import Homedetails from "../Homedetails/Homedetails"
 import {
   useLoaderData
--- src/components/Homedetails/Homedetails.js
+++ src/components/Homedetails/Homedetails.js
@@ -5,15 +5,17 @@ const Homedetails = ({
   const {
     id,
     name,
-    logo,total
+    logo,
+    total
   } = item;
   return (
-    <div>
+    <div className="border rounded-md p-5">
     <h5 className="font-bold text-3xl">{name}</h5>
     <img src={logo} />
-    <p>
+    <p className='font-bold mt-1'>
 Total : {total}
     </p>
+    <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-2">Explore</button>
   </div>
 );
 };
