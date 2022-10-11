5090cf9fcd028888ffdd1f230eda6016ef4b0d61
maruf <dev.maruf112@gmail.com>
2022-10-11 16:13:04 +0600

    Home page data organised

--- src/components/Homedetails/Homedetails.js
+++ src/components/Homedetails/Homedetails.js
@@ -1,23 +1,16 @@
 import React from "react";
-const Homedetails = ({
-  item
-}) => {
-  const {
-    id,
-    name,
-    logo,
-    total
-  } = item;
+const Homedetails = ({ item }) => {
+  const { id, name, logo, total } = item;
   return (
     <div className="border rounded-md p-5">
-    <h5 className="font-bold text-3xl">{name}</h5>
-    <img src={logo} />
-    <p className='font-bold mt-1'>
-Total : {total}
-    </p>
-    <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-2">Explore</button>
-  </div>
-);
+      <h5 className="font-bold text-3xl">{name}</h5>
+      <img style={{ height: "200px", width: "200px" }} className="mx-auto" src={logo} />
+      <p className="font-bold mt-1">Total : {total}</p>
+      <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow my-2">
+        Explore
+      </button>
+    </div>
+  );
 };
 
 export default Homedetails;
