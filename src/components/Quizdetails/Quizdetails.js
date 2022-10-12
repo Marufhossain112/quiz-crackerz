9c78bf20b4bd62fc02b2b72dbd5c3d30b801dd75
maruf <dev.maruf112@gmail.com>
2022-10-12 20:24:35 +0600

    redirect file added

--- src/components/Quizdetails/Quizdetails.js
+++ src/components/Quizdetails/Quizdetails.js
@@ -5,24 +5,25 @@ import icon from "./icon/eye.svg";
 import "./Quizedetails.css";
 
 const Quizdetails = ({ quiz }) => {
-  const [check, setCheck] = useState(true);
   const { correctAnswer, id, question, options } = quiz;
   const getCorrectAns = () => {
     toast(`Correct answer is : ${correctAnswer}`);
   };
   const getValueFromQuiz = (e) => {
     // console.log(e);
+    
     const element = e.target;
     const quizText = element.innerText;
     if (quizText === correctAnswer) {
       toast("Congratulations! You are right.");
-      // setCheck(true);
+      //set color
+      e.target.classList.add("bg-green-500");
     } else {
       toast("Sorry, your answer is wrong.");
-      // setCheck(false);
+      e.target.classList.add("bg-red-500");
     }
-    // console.log(check);
   };
+
   return (
     <div className="bg-violet-50 relative">
       <h3 className="text-xl m-[15px] font-bold pt-5">{question}</h3>
@@ -35,12 +36,12 @@ const Quizdetails = ({ quiz }) => {
       <div className="pb-5">
         {options.map((option) => (
           <div className="flex justify-center">
-            <p
+            <button
               onClick={getValueFromQuiz}
-              className="border border-zinc-600 rounded-lg p-5 w-1/2 my-2 hover:bg-cyan-200 hover:text-black duration-200"
+              className={`border border-zinc-600 rounded-lg p-5 w-1/2 my-2 hover:bg-cyan-200 hover:text-black duration-200}`}
             >
               {option}
-            </p>
+            </button>
             <ToastContainer></ToastContainer>
           </div>
         ))}
