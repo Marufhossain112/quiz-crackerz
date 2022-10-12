42790686db2332114a5c87ffa24056294370d061
maruf <dev.maruf112@gmail.com>
2022-10-12 06:52:20 +0600

    home background pic added

--- src/components/Homebg/Homebg.js
+++ src/components/Homebg/Homebg.js
@@ -1,20 +1,29 @@
 import React from "react";
+import "./Homebg.css";
 const Homebg = () => {
   return (
     <div>
       <div
+        className="mb-10 relative home-bg"
         style={{
           backgroundImage:
             "url(" +
-            "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj3_6PO99j6AhX7bGwGHXqSBlIQ1bUFegQICxAA&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fquiz%2F&usg=AOvVaw3IOE2Pn8iLa29MRUzFsB7A" +
+            "https://cdn.pixabay.com/photo/2022/02/02/10/56/questions-6988157__340.png" +
             ")",
-          backgroundPosition: "center",
+          backgroundPosition: "6px -208px",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
+          height: "100vh",
         }}
       >
-        <h2>I am groot</h2>
-        <p>Best site for Learning.</p>
+        <div className="intro absolute top-[35%] left-[40%]">
+          <h2 className="text-5xl font-bold text-purple-700">
+            Know the Unknown
+          </h2>
+          <p className="text-2xl font-semibold mt-3 text-yellow-300">
+            Best site for Developing Skills.
+          </p>
+        </div>
       </div>
     </div>
   );
